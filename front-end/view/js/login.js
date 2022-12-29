60e7ca87360116ccf80ac26ea8aceb7d2895e10d
JD_Rodrigues <jdevproject01@gmail.com>
2022-12-29 13:29:39 -0300

    Implementation - Tela de login e lógica que mostra a tela sob a condição de o usuário não estar logado na aplicação.

--- front-end/view/js/login.js
+++ front-end/view/js/login.js
@@ -1,11 +1,39 @@
 
 
+
 function handleCredentialResponse(response) {
   const data = jwt_decode(response.credential)
   console.log(data)
-}
-window.onload = function () {
+  localStorage.setItem('authenticated', 'true')
   checkLogin()
+}
+window.onload = () => checkLogin();
+
+const showHome = () => {
+  const body = document.querySelector('body')
+  const homepage = `
+    <header>
+      <input type="search" name="" id="search" oninput=filterCards() />
+      <button id="logout" onclick="logout()">Logout</button>
+    </header>
+    <div id="dTrello"></div>
+    <button id="addNewBoard">Adicionar novo quadro</button>
+  `
+  body.innerHTML = `${homepage}`
+
+}
+
+const showLoginScreen = () => {
+  const body = document.querySelector('body')
+  body.innerHTML = `
+  <div  class="login__modal">
+  <form  class="login__form">
+    <h2 class="login__form__title">Login</h2>
+    <div id="buttonLogin"></div>
+  </form>
+  </div>
+`
+
   google.accounts.id.initialize({
     client_id: "441397660499-jckg2d72sbhi88882vjh8v9n9djhjmg0.apps.googleusercontent.com",
     callback: handleCredentialResponse
@@ -18,25 +46,11 @@ window.onload = function () {
 }
 
 const checkLogin = () => {
-  const loginScreen = `
-      <div  class="login__modal">
-      <form  class="login__form">
-        <h2 class="login__form__title">Login</h2>
-        <div id="buttonLogin"></div>
-      </form>
-      </div>
-    `
-  const homepage = `
-    <header>
-      <input type="search" name="" id="search" oninput=filterCards() />
-    </header>
-    <div id="dTrello"></div>
-    <button id="addNewBoard">Adicionar novo quadro</button>
-  `
-  const body = document.querySelector('body')
-  const credential = localStorage.getItem('credential')
-  // localStorage.removeItem('credential')
-  // localStorage.setItem('credential', JSON.stringify({user: 'JD'}))
+  const authenticated = localStorage.getItem('authenticated')  
+  authenticated === null ? showLoginScreen() : showHome()
+}
 
-  credential === null ? body.innerHTML = `${loginScreen}` : body.innerHTML = `${homepage}`
+const logout = () => {
+  localStorage.removeItem('authenticated')
+  checkLogin()
 }
